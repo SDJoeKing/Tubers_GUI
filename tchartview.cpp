c3383c8bc9eb7a687ebd697387e8f756ba96dacb
Linghao <linghao.zhou@outlook.com>
2024-09-18 22:35:11 +0100

    chartview class

--- tchartview.cpp
+++ tchartview.cpp
@@ -2,7 +2,6 @@
 
 TChartView::TChartView(QWidget *parent) : QChartView{parent}
 {
-
     m_chart = new QChart();
     m_series = new QLineSeries(this);
     m_X = new QValueAxis();
@@ -11,8 +10,57 @@ TChartView::TChartView(QWidget *parent) : QChartView{parent}
     m_chart->addSeries(m_series);
     m_chart->addAxis(m_X, Qt::AlignBottom);
     m_chart->addAxis(m_Y, Qt::AlignLeft);
-
     m_series->attachAxis(m_X);
     m_series->attachAxis(m_Y);
     this->setChart(m_chart);
+
+    this->setContextMenuPolicy(Qt::CustomContextMenu);
+
+    // chart scene
+    // auto frameRect = this->rect();
+    // this->setSceneRect(frameRect.x()+10, frameRect.y()+10, frameRect.height()-20, frameRect.width()-20);
+
+    // label and axis format
+    m_series->setName("A-scan");
+    m_X->setTitleText("A-scan data points");
+    m_Y->setTitleText("Amplitude [mV]");
+    m_X->setLabelFormat("%d");
+    m_X->setRange(xMin, xMax);
+    m_Y->setRange(yMin, yMax);
+
+    // gate
+    m_gate1 = new TGate();
+    m_gate2 = new TGate();
+    m_gate1->setZValue(100);
+    m_gate2->setZValue(100);
+    m_gate1->setVisible(false);
+    m_gate2->setVisible(false);
+    m_gate1->setFlags(m_gate1->flags()| QGraphicsItem::ItemIsSelectable);
+    m_gate2->setFlags(m_gate2->flags()| QGraphicsItem::ItemIsSelectable);
+
+    // add gate to scene
+    this->scene()->addItem(m_gate1);
+    this->scene()->addItem(m_gate2);
+    m_gate1->moveBy(-10, 0);
+    m_gate2->moveBy(10, 0);
+
 }
+
+void TChartView::plot(QList<QPointF> data, bool axisIsDepth)
+{
+
+}
+
+// void TChartView::toogleGates(bool arg)
+// {
+//     m_gate1->setVisible(arg);
+//     m_gate2->setVisible(arg);
+// }
+
+
+// void TChartView::resizeEvent(QResizeEvent *event)
+// {
+//     auto frameRect = this->rect();
+//     this->setSceneRect(frameRect.x()+10, frameRect.y()+10, frameRect.height()-20, frameRect.width()-20);
+//     QChartView::resizeEvent(event);
+// }
--- tchartview.h
+++ tchartview.h
@@ -8,13 +8,17 @@
 #include <QValueAxis>
 
 #include "mtcpclient.h"
+#include "tgate.h"
 
 class TChartView : public QChartView
 {
     Q_OBJECT
 public:
-    explicit TChartView(mTcpClient *client, QWidget *parent = nullptr);
+    explicit TChartView(QWidget *parent = nullptr);
     void plot(QList<QPointF> data, bool axisIsDepth);
+
+public slots:
+    // void toogleGates(bool);
 
 private:
 
@@ -22,7 +26,16 @@ private:
     QLineSeries *m_series;
     QValueAxis *m_X;
     QValueAxis *m_Y;
+    TGate *m_gate1;
+    TGate *m_gate2;
+    float yMin=-20;
+    float yMax = 20;
+    float xMin=0;
+    float xMax = mTcpClient::DATA_SIZE/2;
 
+    // QWidget interface
+protected:
+    // virtual void resizeEvent(QResizeEvent *event) override;
 };
 
 #endif // TCHARTVIEW_H

ecf2fc6854a0f5904a1f16594d96813eaae887f4
SDJoeKing <linghao.zhou@outlook.com>
2024-09-18 09:18:35 +0100

    Delete build directory

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/qtcsettings.cmake
+++
@@ -1,4 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_CXX_COMPILER" "C:/Qt/Tools/mingw1120_64/bin/g++.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/Qt/6.7.2/mingw_64;D:/Qt/6.7.2/mingw_64" CACHE "PATH" "" FORCE)

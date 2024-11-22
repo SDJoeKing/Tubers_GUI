8e528cd9f438a35f49e892dea18eaf451659329e
linghao <linghao.zhou@outlook.com>
2024-11-22 16:58:19 +0000

    threaded processor

---
+++ processor.cpp
@@ -0,0 +1,120 @@
+#include "processor.h"
+#include "mainwindow.h"
+
+QElapsedTimer processTimer;
+Processor::Processor(QObject *parent)
+    : QObject{parent}
+{
+    m_vel = 5890.0;
+    depthAxis = false;
+    rectify = false;
+    filtering = false;
+    qDebug() << "Processor on";
+}
+
+Processor::~Processor()
+{
+    qDebug() << "Processor shutdown";
+    delete m_loop;
+}
+
+void Processor::setParam(const float &vel, const bool &depth, const bool &rect, const bool &filt)
+{
+    m_vel = vel;
+    depthAxis = depth;
+    rectify = rect;
+    filtering = filt;
+}
+
+void Processor::updateFilter(const quint8 &order, const quint8 &fs, const float &fc, const float &fw)
+{
+    m_filter.setup( order, fs, fc, fw);
+}
+
+void Processor::run()
+{
+    m_loop = new QEventLoop();
+
+    m_loop->exec();
+    deleteLater();
+}
+
+void Processor::close()
+{
+    m_loop->quit();
+}
+
+void Processor::process(const char *dataptr)
+{
+    processTimer.restart();
+    auto serverData = QByteArray::fromRawData(dataptr, mTcpClient::DATA_SIZE );
+    quint8 _forward = static_cast<quint8>(serverData.at(4));
+    int j=0;
+    double xpoint=0;
+    QVector<QPointF> calPoint(mTcpClient::DATA_SIZE/2 + 1);
+    quint16 temp1;
+    quint16 temp2;
+    float *dataPoint[1];
+    float _temp[mTcpClient::DATA_SIZE/2]{0};
+    dataPoint[0] = _temp;
+    QByteArray _arr(mTcpClient::DATA_SIZE/2, Qt::Uninitialized);
+
+    bool _tempDepthFlag = false;
+    if(depthAxis)
+        _tempDepthFlag = true;
+
+    for (int i = 0; i < mTcpClient::DATA_SIZE/2; i++)
+    {
+        temp1 =(serverData.at(j + 1) << 8) & 0xFF00;
+        temp2 = (serverData.at(j)) & 0xFF;
+        dataPoint[0][i] = static_cast<qint16>(temp2 | temp1)/ 32768.0  * 3.18 * 1.0 *1000.0;
+
+        j += 2;
+    }
+
+    if(filtering)
+    {
+        m_filter.process(mTcpClient::DATA_SIZE/2, dataPoint);
+    }
+    // rectified, envelope, depth?
+    for (int i = 0; i < mTcpClient::DATA_SIZE/2; i++)
+    {
+        xpoint = i;
+        if(_tempDepthFlag)
+            xpoint = i / 2/ 125e6 * m_vel * 1000;
+
+        if(rectify)
+            dataPoint[0][i] = MainWindow::envelope(dataPoint[0][i], MainWindow::_env, MainWindow::m_ga, MainWindow::m_gr);
+
+        calPoint[i] = QPointF(xpoint, dataPoint[0][i]);
+        _arr[i] = dataPoint[0][i];
+    }
+
+    //reset envelope;
+    MainWindow::_env = 0;
+
+    qDebug() << "Proc: " << processTimer.durationElapsed();
+
+    emit dataLogger(_arr.data());
+    emit dataProcessed(calPoint, _forward == 2 ? false : true);
+}
+
+void Processor::setVel(const float &vel)
+{
+    m_vel = vel;
+}
+
+void Processor::setDepth(const bool &depth)
+{
+    depthAxis = depth;
+}
+
+void Processor::setRectified(const bool &rect)
+{
+    rectify = rect;
+}
+
+void Processor::setFiltering(const bool &filt)
+{
+    filtering = filt;
+}
---
+++ processor.h
@@ -0,0 +1,41 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+#include <QObject>
+#include "DspFilters/Dsp.h"
+#include <QElapsedTimer>
+#include <QEventLoop>
+#include "mtcpclient.h"
+
+class Processor : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Processor(QObject *parent = nullptr);
+    ~Processor();
+    void setParam(const float &vel, const bool &depth, const bool &rect, const bool &filt);
+    void run();
+    void close();
+
+private:
+    Dsp::SimpleFilter<Dsp::Butterworth::BandPass <4> , 1> m_filter;
+    float m_vel;
+    bool depthAxis;
+    bool rectify;
+    bool filtering;
+    QEventLoop *m_loop;
+
+signals:
+    void dataProcessed(const QList<QPointF> &, bool);
+    void dataLogger(const char *);
+
+public slots:
+    void process(const char *);
+    void setVel(const float &vel);
+    void setDepth(const bool &depth);
+    void setRectified(const bool &rect);
+    void setFiltering(const bool &filt);
+    void updateFilter(const quint8 & order, const quint8 &fs, const float &fc, const float &fw);
+};
+
+#endif // PROCESSOR_H

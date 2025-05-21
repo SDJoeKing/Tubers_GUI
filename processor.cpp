460dd25b8e170ecfe1cff423e5e8654ab2a52cc3
linghao <linghao.zhou@outlook.com>
2025-05-21 12:56:46 +0100

    delete debugging msgs

--- processor.cpp
+++ processor.cpp
@@ -126,32 +126,24 @@ void Processor::process(const char *dataptr, bool headerOnly)
     else
         serverData = QByteArray::fromRawData(dataptr, HEADER_SIZE);
 
-    qDebug() << "From Processpr " << serverData;
+
 
     double xpoint=0;
-    qDebug() << "From Processpr " << serverData;
-
     quint16 temp1;
     quint16 temp2;
-    qDebug() << "From Processpr " << serverData;
     float *dataPoint[1];
-    qDebug() << "From Processpr " << serverData;
     float _temp[ DATA_SIZE/2]{0};
 
-
     dataPoint[0] = _temp;
-    qDebug() << "From Processpr " << serverData;
-
-
 
     // take into account of header data
     int j=  HEADER_SIZE;
 
     // get system information from the header data
     quint8 _forward = static_cast<quint8>(serverData.at(HEADER::encoderDirection));
-    qDebug() << "From Processpr " << serverData;
+
     int linkSpeed = (static_cast<quint8>(serverData.at(HEADER::linkSpeed)) * 10);
-    qDebug() << "From Processpr " << serverData;
+
     if(headerOnly)
     {
         qDebug() << serverData;
@@ -229,7 +221,7 @@ void Processor::process(const char *dataptr, bool headerOnly)
 
     // normalise data to (0, 1]
     QList<QPointF> calPoint( DATA_SIZE/2, QPointF(0.0, 0.0));
-    qDebug() << "From Processpr " << serverData;
+
     for(int i = 0; i < DATA_SIZE/2; i++)
     {
 

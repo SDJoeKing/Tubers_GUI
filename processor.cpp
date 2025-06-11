ff7930938eed72ddae16a823af0eaa123801222b
linghao <linghao.zhou@outlook.com>
2025-06-11 15:56:36 +0100

    minor

--- processor.cpp
+++ processor.cpp
@@ -235,7 +235,7 @@ void Processor::process(const char *dataptr, bool headerOnly)
             xpoint = i / m_fs /1e6 *1000;
 
         dataPoint[0][i] /= (m_maxValue / 100 / m_scale);
-        calPoint.append(QPointF(xpoint, dataPoint[0][i]));
+        calPoint.emplace_back(xpoint, dataPoint[0][i]);
         _temp[i] = dataPoint[0][i];
     }
 
